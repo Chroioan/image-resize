eb4db03003b7f9e93c5e3e9fb9bf3102262ca2a2
Chroioan <116485878+Chroioan@users.noreply.github.com>
2022-10-23 14:05:52 +0300

    Update func.py

--- func.py
+++ func.py
@@ -14,12 +14,12 @@ if not os.path.exists(in_directory):
     os.makedirs(in_directory)
 if not os.path.exists(out_directory):
     os.makedirs(out_directory)
-
-for file in os.listdir(in_directory):
-    in_file = Image.open(f'{in_directory}{file}')
-    in_file.thumbnail(out_size)
-    in_file.save(f'{out_directory}{file}', 'png')
-    print(f' Your image is in {out_directory} directory! \n '
-          f' The size is {in_file.size}')
+if len(os.listdir(in_directory))>0:
+    for file in os.listdir(in_directory):
+        in_file = Image.open(f'{in_directory}{file}')
+        in_file.thumbnail(out_size)
+        in_file.save(f'{out_directory}{file}', 'png')
+        print(f' Your image is in {out_directory} directory! \n '
+              f' The size is {in_file.size}')
 else:
     print('no such a file in direcotry')
